63309401adc85788adf6fd34f9229a89f5eb932e
Luke Jones <me@lukejones.me>
2012-02-10 09:39:26 +0000

    Indent Google Analytics correctly.

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
 	<script src="http://use.typekit.com/feu7nwp.js"></script>
 	<script>try{Typekit.load();}catch(e){}</script>
 	<script>
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-6426998-22']);
-	_gaq.push(['_trackPageview']);
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-6426998-22']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
 	</script>
 
 </head>
